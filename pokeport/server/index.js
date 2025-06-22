8c7f5f401097b343ebb0f7ed8e0bb307c3b7d27b
Eldrish <eldrish.ramosperez@gmail.com>
2025-06-21 18:22:53 -0700

    refresh fix

--- pokeport/server/index.js
+++ pokeport/server/index.js
@@ -6,6 +6,8 @@ import typeDefs from './schema/typeDefs.js'
 import resolvers from './schema/resolvers.js'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config() // Load .env
 
@@ -40,6 +42,17 @@ await server.start()
 server.applyMiddleware({ app })
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const clientBuildPath = path.join(__dirname, '../dist')
+
+// Serve static files from the Vite build
+app.use(express.static(clientBuildPath))
+
+// Serve index.html for all non-API routes (SPA fallback)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'))
+})
 
 app.listen({ port: 4000 }, () =>
   console.log('Server ready at http://localhost:4000' + server.graphqlPath)
